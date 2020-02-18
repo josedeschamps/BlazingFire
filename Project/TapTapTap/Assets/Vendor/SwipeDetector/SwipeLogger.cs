0a49ff5fa85613b2646c0dd0c46b5ca62301a7e9
Jose The Tech Monk <deschamps.jose001@gmail.com>
2020-02-18 12:06:30 -0500

    Updated Unity

--- Project/TapTapTap/Assets/Vendor/SwipeDetector/SwipeLogger.cs
+++ Project/TapTapTap/Assets/Vendor/SwipeDetector/SwipeLogger.cs
@@ -2,13 +2,19 @@
 
 public class SwipeLogger : MonoBehaviour
 {
+
+
+    
     private void Awake()
     {
         SwipeDetector.OnSwipe += SwipeDetector_OnSwipe;
+  
     }
 
     private void SwipeDetector_OnSwipe(SwipeData data)
     {
         Debug.Log("Swipe in Direction: " + data.Direction);
     }
+
+    
 }
